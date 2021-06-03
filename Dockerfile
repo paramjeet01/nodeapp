bd2e6e32ce440e8cb88835f3c379e850669f7db3
Paramjeet <paramjeet.b@coda.global>
2021-06-03 19:02:39 +0530

    exp

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM node:16-alpine3.11
 COPY nodeapp.js .
-EXPOSE 8080
+EXPOSE 8001
 CMD [ "node", "nodeapp.js"]
--- nodeapp.js
+++ nodeapp.js
@@ -3,4 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(8080);
+}).listen(8001);
+
+console.log('Server running at http://127.0.0.1:8001/');
